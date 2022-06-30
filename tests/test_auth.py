64c3d370e691d9fa771bccd18d065f833d1ca3f8
guan7008 <woo@iappsasia.com>
2022-06-30 18:46:34 +0800

    test: Update google login tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -2,7 +2,28 @@ import os
 import requests
 
 
-def test_google_login_no_header(api_v1_host):
-    endpoint = os.path.join(api_v1_host, "auth", "google", "login")
+def test_google_login_no_header():
+    endpoint = "http://localhost:8000/api/v1/auth/google/login"
     response = requests.post(endpoint)
-    assert response.status_code == 401
+    assert response.status_code == 200
+    json = response.json()
+    assert "status_code" in json
+    assert "message" in json
+    assert json["status_code"] == 20001
+    assert json["message"] == "ERROR: Unauthorized"
+
+
+def test_google_login_no_data():
+    endpoint = "http://localhost:8000/api/v1/auth/google/login"
+    response = requests.post(
+        endpoint,
+        headers={
+            "x-api-key": "5cebc2dc-894e-4ce9-ae13-e446c348f7e6",
+        },
+    )
+    assert response.status_code == 200
+    json = response.json()
+    assert "status_code" in json
+    assert "message" in json
+    assert json["status_code"] == 21001
+    assert json["message"] == "ERROR: Invalid Input"
