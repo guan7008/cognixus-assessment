a64fe06de82358f2fc8b17061fa6bbc5f4854e70
guan7008 <woo@iappsasia.com>
2022-06-30 18:49:01 +0800

    test: Add google login complete

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -27,3 +27,22 @@ def test_google_login_no_data():
     assert "message" in json
     assert json["status_code"] == 21001
     assert json["message"] == "ERROR: Invalid Input"
+
+def test_google_login_complete():
+    endpoint = "http://localhost:8000/api/v1/auth/google/login"
+    response = requests.post(
+        endpoint,
+        data={
+            "redirect_uri": "http://localhost:8000/google/auth"
+        },
+        headers={
+            "x-api-key": "5cebc2dc-894e-4ce9-ae13-e446c348f7e6",
+        },
+    )
+    assert response.status_code == 200
+    json = response.json()
+    assert "status_code" in json
+    assert "message" in json
+    assert "redirect_uri" in json
+    assert json["status_code"] == 20000
+    assert json["message"] == "OK: Proceed To Login Uri"
