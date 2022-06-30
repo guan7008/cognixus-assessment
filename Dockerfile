673ed6c9852991b28e743bdbccccdbade9af808a
guan7008 <woo@iappsasia.com>
2022-06-30 11:33:04 +0800

    feat: Add Google login

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,14 @@ ENV MYSQL_PORT=3306
 ENV MYSQL_DATABASE=cadb
 ENV MYSQL_USER=root
 ENV MYSQL_PASSWORD=r00t
-RUN apk add --no-cache gcc musl-dev linux-headers
+ENV GOOGLE_CLIENT_ID=47725177727-d0hia48tklelj658f6hju223udehs9bn.apps.googleusercontent.com
+ENV GOOGLE_PROJECT_ID=sunlit-monolith-212409
+ENV GOOGLE_AUTH_URI=https://accounts.google.com/o/oauth2/auth
+ENV GOOGLE_TOKEN_URI=https://oauth2.googleapis.com/token
+ENV GOOGLE_AUTH_PROVIDER_X509_CERT_URL=https://www.googleapis.com/oauth2/v1/certs
+ENV GOOGLE_CLIENT_SECRET=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
+ENV GOOGLE_REDIRECT_URI=http://localhost:8000/google/auth
+RUN apk add --no-cache gcc musl-dev linux-headers libffi-dev
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 EXPOSE 5000
--- src/endpoints/blueprint_auth.py
+++ src/endpoints/blueprint_auth.py
@@ -1,15 +1,49 @@
+import os
+import validators
+import google.oauth2.credentials
+import googleapiclient.discovery
 from crypt import methods
 from flask import Blueprint, jsonify, request
+from authlib.client import OAuth2Session
 
 # define the blueprint
 blueprint_auth = Blueprint(name="blueprint_auth", import_name=__name__)
 
+GOOGLE_CLIENT_ID = os.environ["GOOGLE_CLIENT_ID"]
+GOOGLE_PROJECT_ID = os.environ["GOOGLE_PROJECT_ID"]
+GOOGLE_AUTH_URI = os.environ["GOOGLE_AUTH_URI"]
+GOOGLE_TOKEN_URI = os.environ["GOOGLE_TOKEN_URI"]
+GOOGLE_AUTH_PROVIDER_X509_CERT_URL = os.environ["GOOGLE_AUTH_PROVIDER_X509_CERT_URL"]
+GOOGLE_CLIENT_SECRET = os.environ["GOOGLE_CLIENT_SECRET"]
+GOOGLE_REDIRECT_URI = os.environ["GOOGLE_REDIRECT_URI"]
+AUTHORIZATION_SCOPE = "openid email profile"
 
-@blueprint_auth.route("/login", methods=["POST"])
-def login():
+
+@blueprint_auth.route("/google/login", methods=["POST"])
+def google_login():
+    # retrieve body data from input JSON
+    redirect_uri = request.form.get("redirect_uri")
+
+    if not redirect_uri or not validators.url(redirect_uri):
+        return jsonify(
+            {
+                "message": "ERROR: Invalid Input",
+            },
+            400,
+        )
+
+    session = OAuth2Session(
+        GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET,
+        scope=AUTHORIZATION_SCOPE,
+        redirect_uri=redirect_uri,
+    )
+
+    uri = session.authorization_url(GOOGLE_AUTH_URI)
     return jsonify(
         {
-            "message": "OK: Authorized",
+            "message": "OK: Proceed To Login Uri",
+            "redirect_uri": uri,
         },
         200,
     )
